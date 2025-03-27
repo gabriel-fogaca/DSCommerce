9bef126001ba3045fec6df508dce7318c1dc3fae
gabriel-fogaca <l44.gabriel@hotmail.com>
2025-03-27 13:06:22 -0300

    OrderDTO, get order by id

---
+++ src/main/java/com/devsuperior/dscommerce/dto/OrderDTO.java
@@ -0,0 +1,70 @@
+package com.devsuperior.dscommerce.dto;
+
+import com.devsuperior.dscommerce.entities.Order;
+import com.devsuperior.dscommerce.entities.OrderItem;
+import com.devsuperior.dscommerce.entities.OrderStatus;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderDTO {
+    private Long id;
+    private Instant moment;
+    private OrderStatus status;
+    private ClientDTO client;
+    private PaymentDTO paymentDTO;
+    private List<OrderItemDTO> items = new ArrayList<>();
+
+    public OrderDTO(Long id, Instant moment, OrderStatus status, ClientDTO client, PaymentDTO paymentDTO) {
+        this.id = id;
+        this.moment = moment;
+        this.status = status;
+        this.client = client;
+        this.paymentDTO = paymentDTO;
+    }
+
+    public OrderDTO(Order entity) {
+        this.id = entity.getId();
+        this.moment = entity.getMoment();
+        this.status = entity.getStatus();
+        this.client = new ClientDTO(entity.getClient());
+        this.paymentDTO = (entity.getPayment() == null) ? null : new PaymentDTO(entity.getPayment());
+        for (OrderItem item : entity.getItems()) {
+            OrderItemDTO itemDTO = new OrderItemDTO(item);
+            items.add(itemDTO);
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public ClientDTO getClient() {
+        return client;
+    }
+
+    public PaymentDTO getPaymentDTO() {
+        return paymentDTO;
+    }
+
+    public List<OrderItemDTO> getItems() {
+        return items;
+    }
+
+    public Double getTotal() {
+        double sum = 0.0;
+        for (OrderItemDTO item : items) {
+            sum += item.getSubTotal();
+        }
+        return sum;
+    }
+}
